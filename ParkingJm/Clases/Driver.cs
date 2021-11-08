734e4894a26375dbdfcfa7e48ac781114ed7f344
Juan Felipe Meza Morales <juan.meza.2021@upb.edu.co>
2021-11-07 21:58:08 -0500

    Agregar archivos de proyecto.

---
+++ ParkingJm/Clases/Driver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingJm.Clases
+{
+    class Driver
+    {
+        public string Nombre;
+        public string Identificacion;
+        public string Sexo;
+        public bool Afiliado;
+
+    }
+}
